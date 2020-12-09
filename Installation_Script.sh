12850275df1a2551372afb7ece7b8def47d15be9
Gonca Atabek Uz <43034144+gonjumixproject@users.noreply.github.com>
2020-12-09 12:08:18 +0300

    Update Installation_Script.sh

--- Installation_Script.sh
+++ Installation_Script.sh
@@ -36,15 +36,15 @@ while true; do
 
                                                         #You must have older php5.6 for the FreePBX 15 usage
 
-                                                        apt-get install -y openssh-server apache2 mysql-server mysql-client \
-                                                                      mongodb curl sox mpg123 sqlite3 git uuid libodbc1 unixodbc unixodbc-bin \
-                                                                        asterisk asterisk-core-sounds-en-wav asterisk-core-sounds-en-g722 \
-                                                                          asterisk-dahdi asterisk-flite asterisk-modules asterisk-mp3 asterisk-mysql \
-                                                                            asterisk-moh-opsound-g722 asterisk-moh-opsound-wav asterisk-opus \
-                                                                              asterisk-voicemail dahdi dahdi-dkms dahdi-linux libapache2-mod-security2 \
-                                                                                php5.6 php5.6-cgi php5.6-cli php5.6-curl php5.6-fpm php5.6-gd php5.6-mbstring \
-                                                                                  php5.6-mysql php5.6-odbc php5.6-xml php5.6-bcmath php-pear libicu-dev gcc \
-                                                                                    g++ make postfix libapache2-mod-php5.6
+                                                         apt-get install -y openssh-server apache2 mysql-server mysql-client 
+                                                         apt-get install -y mongodb curl sox mpg123 sqlite3 git uuid libodbc1 unixodbc
+                                                         apt-get install -y asterisk asterisk-core-sounds-en-wav asterisk-core-sounds-en-g722 
+                                                         apt-get install -y asterisk-dahdi asterisk-flite asterisk-modules asterisk-mp3 asterisk-mysql
+                                                         apt-get install -y asterisk-moh-opsound-g722 asterisk-moh-opsound-wav asterisk-opus 
+                                                         apt-get install -y asterisk-voicemail dahdi dahdi-dkms dahdi-linux libapache2-mod-security2 
+                                                         apt-get install -y php5.6 php5.6-cgi php5.6-cli php5.6-curl php5.6-fpm php5.6-gd php5.6-mbstring 
+                                                         apt-get install -y php5.6-mysql php5.6-odbc php5.6-xml php5.6-bcmath php-pear libicu-dev gcc 
+                                                         apt-get install -y g++ make postfix libapache2-mod-php5.6
 
                                                           #As part of this install, you may be asked (possibly several times) for a mysql password. DO NOT SET A MYSQL PASSWORD AT THIS POINT.
 
